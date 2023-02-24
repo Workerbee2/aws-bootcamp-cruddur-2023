5e217047a02d60bde96cab0080e0540443ce49d1
Msaghu <maureenmwagoti@gmail.com>
2023-02-24 18:47:09 +0000

    installed Postgres and DynamoDB

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -7,7 +7,7 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 
-// [TODO] Authenication
+// [TODO] Authentication
 import Cookies from 'js-cookie'
 
 export default function NotificationsFeedPage() {
