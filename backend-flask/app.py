ed3903a28b4a616cea06fd85116184f71f391850
Msaghu <maureenmwagoti@gmail.com>
2023-03-08 18:39:34 +0000

    Docker compose change

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -35,7 +35,6 @@ provider.add_span_processor(processor)
 #Xray 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 #Shows output in the logs for the backend flask logs
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -45,6 +44,10 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+#X Ray
+XRayMiddleware(app, xray_recorder)
+
 
 #Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -49,5 +49,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-    span.set_attribute("app.result_length", len(results.length))
+    span.set_attribute("app.result_length", len(results))
     return results
