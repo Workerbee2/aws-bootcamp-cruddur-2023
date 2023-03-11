8f4c99d587fbbea8d1b2d85c311f88d675f4dbbc
Msaghu <maureenmwagoti@gmail.com>
2023-03-11 16:42:50 +0000

    Update Docker variables and sign in sign up pages

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -35,32 +35,19 @@ export default function ConfirmationPage() {
       if (err.message == 'Username cannot be empty'){
         setCognitoErrors("You need to provide an email in order to send Resend Activiation Code")   
       } else if (err.message == "Username/client id combination not found."){
-        setCognitoErrors("Email is invalid or cannot be found.")   
+        setErrors("Email is invalid or cannot be found.")   
       }
     }
   }
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setCognitoErrors('')
+    setErrors('')
     try {
-        const { user } = await Auth.signUp({
-          username: email,
-          password: password,
-          attributes: {
-              name: name,
-              email: email,
-              preferred_username: username,
-          },
-          autoSignIn: { // optional - enables auto sign in after user is confirmed
-              enabled: true,
-          }
-        });
-        console.log(user);
-        window.location.href = `/confirm?email=${email}`
+      await Auth.confirmSignUp(email, code);
+      window.location.href = "/"
     } catch (error) {
-        console.log(error);
-        setCognitoErrors(error.message)
+      setErrors(error.message)
     }
     return false
   }
