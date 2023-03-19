5492481205721cf104e4f534c09bf8e27fcd1470
Msaghu <maureenmwagoti@gmail.com>
2023-03-19 19:43:09 +0000

    change in week 2 code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -102,7 +102,7 @@ cors = CORS(
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
 
-# Rollbar ----------
+#Rollbar -initialization by setting env var
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 @app.before_first_request
 def init_rollbar():
